be1418b3f46e19db105146abb967586664619732
jimmykodes <james.keith.92@gmail.com>
2020-07-13 06:49:11 -0700

    add fractal clock

---
+++ fractal_clock/clock.js
@@ -0,0 +1,88 @@
+const TAU = Math.PI * 2
+
+function Point(x, y) {
+    this.x = x
+    this.y = y
+}
+
+function Clock(depth, canvas, ctx, cx, cy, r, offset) {
+    this.depth = depth
+    this.canvas = canvas
+    this.ctx = ctx
+    this.cx = cx
+    this.cy = cy
+    this.r = r
+    this.offset = offset
+    this.h = undefined
+    this.m = undefined
+    this.s = undefined
+    this.ms = undefined
+    this.hRad = undefined
+    this.mRad = undefined
+    this.sRad = undefined
+    this.hDeg = undefined
+    this.mDeg = undefined
+    this.sDeg = undefined
+    this.hChild = undefined
+    this.mChild = undefined
+    this.sChild = undefined
+    this.maxDepth = 8
+}
+
+Clock.prototype.updateTime = function (now, cx, cy, offset) {
+    now = now || new Date()
+    this.cx = cx || this.cx
+    this.cy = cy || this.cy
+    this.offset = offset || this.offset
+    this.ms = now.getMilliseconds()
+    this.s = (now.getSeconds() + (this.ms / 1000))
+    this.m = (now.getMinutes() + (this.s / 60))
+    this.h = (now.getHours() + (this.m / 60))
+    this.hRad = (this.h * TAU / 12) + this.offset - (Math.PI / 2)
+    this.mRad = (this.m * TAU / 60) + this.offset - (Math.PI / 2)
+    this.sRad = (this.s * TAU / 60) + this.offset - (Math.PI / 2)
+    this.hDeg = this.radToDeg(this.hRad)
+    this.mDeg = this.radToDeg(this.mRad)
+    this.sDeg = this.radToDeg(this.sRad)
+    if (this.depth > this.maxDepth) {
+        return
+    }
+    if (!this.hChild) {
+        this.hChild = new Clock(this.depth + 1, this.canvas, this.ctx, this.hDeg.x, this.hDeg.y, this.r * .7, this.hRad)
+    }
+    if (!this.mChild) {
+        this.mChild = new Clock(this.depth + 1, this.canvas, this.ctx, this.mDeg.x, this.mDeg.y, this.r * .7, this.mRad)
+    }
+    if (!this.sChild) {
+        this.sChild = new Clock(this.depth + 1, this.canvas, this.ctx, this.sDeg.x, this.sDeg.y, this.r * .7, this.sRad)
+    }
+    this.hChild.updateTime(now, this.hDeg.x, this.hDeg.y, this.hRad)
+    this.mChild.updateTime(now, this.mDeg.x, this.mDeg.y, this.mRad)
+    this.sChild.updateTime(now, this.sDeg.x, this.sDeg.y, this.sRad)
+}
+
+Clock.prototype.radToDeg = function (theta) {
+    return new Point((Math.cos(theta) * this.r) + this.cx, (Math.sin(theta) * this.r) + this.cy)
+}
+
+Clock.prototype.draw = function () {
+    this.updateTime()
+    let ctx = this.ctx
+    if (this.depth === 0) {
+        ctx.save()
+        ctx.beginPath()
+    }
+    [this.mDeg, this.sDeg].forEach(point => {
+        ctx.moveTo(this.cx, this.cy)
+        ctx.lineTo(point.x, point.y)
+    })
+    if (this.depth > this.maxDepth) {
+        return
+    }
+    [this.mChild, this.sChild].forEach(c => c.draw())
+
+    if (this.depth === 0) {
+        ctx.stroke()
+        ctx.restore()
+    }
+}
---
+++ fractal_clock/fractal.js
@@ -0,0 +1,32 @@
+function Canvas() {
+    this.width = window.innerWidth
+    this.height = window.innerHeight
+    this.canvas = document.getElementById('canvas')
+    this.ctx = this.canvas.getContext('2d')
+    let dpr = window.devicePixelRatio
+    this.canvas.width = this.width * dpr
+    this.canvas.height = this.height * dpr
+    this.canvas.style.width = `${this.width}px`
+    this.canvas.style.height = `${this.height}px`
+    this.ctx.scale(dpr, dpr)
+    this.clock = new Clock(
+        0,
+        this.canvas,
+        this.ctx,
+        this.width / 2,
+        this.height / 2,
+        Math.min(this.width, this.height) * .3,
+        0
+    )
+}
+
+Canvas.prototype.animate = function () {
+    this.ctx.clearRect(0, 0, this.width, this.height)
+    this.clock.draw()
+    window.requestAnimationFrame(this.animate.bind(this))
+}
+
+$(document).ready(() => {
+    let c = new Canvas()
+    window.requestAnimationFrame(c.animate.bind(c))
+})
